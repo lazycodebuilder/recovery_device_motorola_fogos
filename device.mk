f0128ef095c6742b462c92893b035125809aa430
lazycodebuilder <154446636+lazycodebuilder@users.noreply.github.com>
2024-05-09 11:04:59 +0530

    Removing PRODUCT_SOONG_NAMESPACES flag

--- device.mk
+++ device.mk
@@ -64,6 +64,3 @@ PRODUCT_EXTRA_RECOVERY_KEYS += \
 # Copy modules for depmod
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*.ko,$(DEVICE_PATH)/prebuilt/modules,$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/1.1)
 
-# Soong namespaces
-PRODUCT_SOONG_NAMESPACES += \
-    $(DEVICE_PATH)
